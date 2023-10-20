d43c77bca88a4452703f75bf47c4e9ca9837cea3
Denis Anisimov <bikemandan@yandex.ru>
2023-10-20 21:02:46 +0500

    Modified interface "stack_management.h": replaced stack structure internals to a pointer to struct stack (i.e descriptor) so that a user cannot directly access struct stack's fields. Also moved realization specific defines to stack_management.c from the interface. The realization of interface and client program have been changed accordingly. Edited .gitignore so that shell scripts and object files after compilation won't be tracked by Git.

--- pointers/421/stack_management.h
+++ pointers/421/stack_management.h
@@ -2,20 +2,14 @@
 #include <stdlib.h>
 #ifndef STACK_MANAGEMENT
 #define STACK_MANAGEMENT
-#define STACK_SIZE 6
-#define MUL_SIZE 2
 
-struct stack {
-    double *data;      // array numbers
-    int size;          // size array 
-    int top;           // pointer to top of stack
-};
+typedef struct stack *Stack;
 
-struct stack *stackdbl_init();
-void stackdbl_destroy(struct stack *st);
-int stackdbl_empty(struct stack *st);
-struct stack *stackdbl_resize(struct stack *st);
-int stackdbl_push(struct stack *st, double n);
-double stackdbl_pop(struct stack *st);
+Stack stackdbl_init();
+void stackdbl_destroy(Stack);
+int stackdbl_empty(Stack);
+Stack stackdbl_resize(Stack);
+int stackdbl_push(Stack, double);
+double stackdbl_pop(Stack);
 
 #endif
