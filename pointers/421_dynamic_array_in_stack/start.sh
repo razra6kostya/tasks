b61f1b4a08bff93602565dbd732c20dcb221a010
Razrab Kostya <konstantin.kluikin89@gmail.com>
2025-06-23 15:06:03 +0300

    refactoring

--- pointers/421_dynamic_array_in_stack/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f "task421" ]; then
-    make clean
-fi
-make run
-./task421
--- pointers/421_dynamic_array_in_stack/task421.c
+++ pointers/421_dynamic_array_in_stack/task421.c
@@ -7,9 +7,11 @@ int main()
     int i;
     double n;
     Stack stack = stackdbl_init();
-    for (i = 1; i < 30; i++) {
+    for (i = 1; i < 5; i++) {
         stackdbl_push(stack, sin(i));
+        printf("%g\n", sin(i));
     }
+    printf("--------\n");
     while ((n = stackdbl_pop(stack))) {
         printf("%g\n", n);
     }
