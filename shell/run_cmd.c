ef3c918fa3eda1bedac2c3160cabbe2906f5b09d
Razrab Kostya <konstantin.kluikin89@gmail.com>
2025-07-06 20:05:36 +0300

    implementation cmd: CD

--- shell/run_cmd.c
+++ shell/run_cmd.c
@@ -2,6 +2,7 @@
 #include "struct_data.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
@@ -18,22 +19,43 @@ static int number_word(Word_item *word_item)
 static int fork_and_exe(char **cmdline)
 {
     int pid;
-    pid = fork();
-    if (pid == -1) {
-        perror("fork\n"); 
-        exit(1);
+    char *name = "cd\0";
+    if (strcmp(cmdline[0], name) == 0) {
+        if (cmdline[2] == NULL) {
+            if (cmdline[1] == NULL) {
+                chdir(getenv("HOME"));
+                return -2;
+            } else {
+                chdir(cmdline[1]);
+                return -2;
+            }
+        } else {
+            printf("enter extra arg.\n");
+            return -3;
+        }
+    } else {
+        pid = fork();
+        if (pid == -1) {
+            perror("fork\n"); 
+            exit(1);
+        }
+        if (pid == 0) {
+                execvp(cmdline[0], cmdline);
+                perror(cmdline[0]);
+                exit(1);
+        } 
     }
-    if (pid == 0) {
-        execvp(cmdline[0], cmdline);
-        perror(cmdline[0]);
-        exit(1);
-    }
+    
     return pid;
 }
 
 static int wait_procces(int pid)
 {
     int wr, status;
+    if (pid == -2 || pid == -3) {
+        /* cmd = cd */
+        return 0;
+    }
     wr = wait(&status);
     if(wr == -1) {
         printf("There are no child processes at all\n");
