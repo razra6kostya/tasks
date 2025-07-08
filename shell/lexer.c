dc18b9b45b7b1a64cd7ae4436fdba70071117823
Razrab Kostya <konstantin.kluikin89@gmail.com>
2025-07-08 22:32:20 +0300

    rework lexer

---
+++ shell/lexer.c
@@ -0,0 +1,167 @@
+#include "lexer.h"
+#include "struct_data.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define UNMATH_QUOTE 2;
+
+typedef enum {
+    TOKEN_WORD,           // "ls", "-l", "file.txt"
+    TOKEN_PIPE,           // '|'
+    TOKEN_REDIRECT_IN,    // '<'
+    TOKEN_REDIRECT_OUT,   // '>'
+    TOKEN_DOUBLE_REDIRECT_OUT, // '>>'
+    TOKEN_SEMICOLON,      // ';'
+    TOKEN_AMPERSAND,      // '&'
+    TOKEN_LOGICAL_AND,    // '&&'
+    TOKEN_LOGICAL_OR,     // '||'
+    TOKEN_LPAREN,         // '('
+    TOKEN_RPAREN,         // ')'
+    TOKEN_ERROR,          // Error lexical  analysis
+    TOKEN_EOF,            // End of file
+    TOKEN_EOL             // "\n"
+} token_type;
+
+typedef struct {
+    token_type type;
+    char *value;
+} Token;
+
+Token *create_token(token_type type, const char *value)
+{
+    Token *token = (Token *)malloc(sizeof(Token));
+    token->type = type;
+    token->value = (value != NULL) ? strdup(value) : NULL;
+    return token;
+}
+
+Token *get_next_token()
+{
+    return create_token(TOKEN_ERROR, "Unknowd Error\n");
+}
+
+
+
+
+
+
+
+
+
+
+static Mode change_anal_mode(Mode mode)
+{
+    return mode == NORMAL ? INSIDE : NORMAL;
+}
+
+static int switching_mode(Analysis_mode *a_mode, const int ch)
+{
+    if (ch == '"') {
+        a_mode->mode = 
+            change_anal_mode(a_mode->mode);
+        a_mode->quote_count++;
+        return 1;
+    } 
+    return 0;
+}
+
+static int buf_is_full(Buf_word_for_list *wfl)
+{
+    return wfl->cap_len_word - 1 == 
+        wfl->lenght_word;
+}
+
+static void increase_word_buffer(Buf_word_for_list *wfl)
+{
+    if(buf_is_full(wfl)) {
+        wfl->cap_len_word *= 2;
+        char *new_word = 
+            realloc(wfl->b_word, wfl->cap_len_word);
+        if (!new_word) {
+            free(wfl->b_word);
+            return;
+        }
+        wfl->b_word =  new_word;
+    }
+}
+
+static void add_word_in_list(Word_list *wlst)
+{
+    if (wlst->first_item == NULL) {
+        wlst->first_item = malloc(sizeof(Word_item));
+        wlst->last_item = wlst->first_item;
+    } else {
+        wlst->last_item->next = malloc(sizeof(Word_item));
+        wlst->last_item = wlst->last_item->next;
+    }
+    wlst->last_item->word = strdup(wlst->buf_word->b_word);
+    wlst->last_item->next = NULL; 
+}
+
+static void finalize_word(Word_list *wlst)
+{
+    wlst->buf_word->b_word[wlst->buf_word->lenght_word] = '\0';
+    add_word_in_list(wlst);
+    wlst->buf_word->lenght_word = 0;
+}
+
+static int progress_space(const int prev_ch, const int ch)
+{
+    return isspace(prev_ch) && isspace(ch);
+}
+    
+static int handle_end_word(Word_list *wlst, const int ch) 
+{
+    if (isspace(ch)) {
+        switch (wlst->anal_mode->mode) {
+        case NORMAL:
+            if (progress_space(wlst->buf_word->prev_ch, ch)) {
+                /* this word after INSIDE mode */
+                if (wlst->buf_word->lenght_word > 0) {
+                    if (wlst->buf_word->b_word[wlst->buf_word->lenght_word - 1] == ' ') {
+                        ;
+                    }
+                } else {
+                    return 1;
+                }
+            }
+            finalize_word(wlst);
+            return 1; 
+        case INSIDE:
+            if (ch == '\n') {
+                finalize_word(wlst);
+                return 1;
+            }
+            wlst->buf_word->b_word[wlst->buf_word->lenght_word] = ch;
+            break;
+        }
+    }
+    return 0;
+}
+
+void get_character_to_word(Word_list *wlst, const int ch)
+{
+    /* ch == '"'*/
+    if (switching_mode(wlst->anal_mode, ch)) 
+        return;
+
+    increase_word_buffer(wlst->buf_word);
+
+    if (handle_end_word(wlst, ch)) {
+        wlst->buf_word->prev_ch = ch;
+        return;
+    }
+
+    /* normal character */
+    wlst->buf_word->b_word[wlst->buf_word->lenght_word] = ch;
+    wlst->buf_word->lenght_word++;
+    wlst->buf_word->prev_ch = ch;
+}
+
+int quote_count(Word_list *wlst)
+{
+    return wlst->anal_mode->quote_count % 2 == 0;
+}
---
+++ shell/lexer.h
@@ -0,0 +1,8 @@
+#include "struct_data.h"
+#ifndef LEXER
+#define LEXER 
+
+void get_character_to_word(Word_list *wlst, const int ch);
+int quote_count(Word_list *wlst);
+
+#endif
